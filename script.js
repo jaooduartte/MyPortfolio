cce5651d4ed225c3efdcd35ea0c090574fdf0cd4
João Duarte <jpdx2010@hotmail.com>
2023-08-28 18:26:29 -0300

    Envios

---
+++ script.js
@@ -0,0 +1,35 @@
+// ANIMAÇÃO DE DIGITAÇAO NO TEXTO h2
+function escrevendoLetra() {
+    function ativaLetra(elemento) {
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((letra, i) => {
+            setTimeout(() => {
+                elemento.innerHTML += letra;
+            }, 75 * i);
+        });
+    }
+
+    const titulo = document.querySelector('.digitando');
+    ativaLetra(titulo);
+}
+// ANIMAÇÃO DE DIGITAÇAO NO PARAGRAFO
+function escrevendoLetraParagrafo() {
+    function ativaLetraParagrafo(elemento) {
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((letra, i) => {
+            setTimeout(() => {
+                elemento.innerHTML += letra;
+            }, 30 * i);
+        });
+    }
+    const paragrafo = document.querySelector('.descricao');
+    ativaLetraParagrafo(paragrafo);
+}
+
+const ativaMenu = document.querySelector('.fa-bars');
+
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+})

6f93e9d7c541d2827c2ecb0f18d42aeccbcc8878
João Duarte <jpdx2010@hotmail.com>
2023-08-31 02:54:06 -0300

    Alterações

--- script.js
+++ script.js
@@ -29,7 +29,9 @@ function escrevendoLetraParagrafo() {
 }
 
 const ativaMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .navegacao-primaria')
 
 ativaMenu.addEventListener('click', ()=>{
     ativaMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
 })
